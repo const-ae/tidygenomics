13cc9adce729c64c99be674c14aa8ac34ff4d47d
Artjom31415 <artjom31415@googlemail.com>
2017-05-07 22:16:18 +0200

    Replace sort_index function
    - The previous version used some features from C++11 which caused problems
    - I replaced it with a version using less advanced features

--- src/cluster_interval.cpp
+++ src/cluster_interval.cpp
@@ -1,28 +1,83 @@
 #include <Rcpp.h>
 
-// Enable C++11 via this plugin (Rcpp 0.10.3 or later)
-// [[Rcpp::plugins(cpp11)]]
 
 using namespace Rcpp;
 using namespace std;
 
 
-// Function copied from http://stackoverflow.com/a/12399290/604854
-// Modified to handle the RTYPE
+// The following code was copied from the stan math library
+// https://github.com/stan-dev/stan/blob/e118db2b78ed33c40f7b5c774f3ce5b85aa5dfdf/src/stan/math/matrix/sort_indices.hpp
 
-template <int RTYPE>
-vector<size_t> sort_indexes(const Vector<RTYPE>& v) {
+/*
+ * Copyright (c) 2011--2015, Stan Developers and their Assignees
+ All rights reserved.
 
-  // initialize original index locations
-  vector<size_t> idx(v.size());
-  iota(idx.begin(), idx.end(), 0);
+ Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
-  // sort indexes based on comparing values in v
-  sort(idx.begin(), idx.end(),
-       [&v](size_t i1, size_t i2) {return v[i1] < v[i2];});
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * Neither the name of Columbia University nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 
-  return idx;
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+template <bool ascending, typename C>
+class index_comparator {
+  const C& xs_;
+public:
+  /**
+  * Construct an index comparator holding a reference
+  * to the specified container.
+  *
+  * @patam xs Container
+  */
+  index_comparator(const C& xs) : xs_(xs) { }
+
+  /**
+  * Return true if the value at the first index is sorted in
+  * front of the value at the second index;  this will depend
+  * on the template parameter <code>ascending</code>.
+  *
+  * @param i Index of first value for comparison
+  * @param j Index of second value for comparison
+  */
+  bool operator()(int i, int j) const {
+    if (ascending)
+      return xs_[i] < xs_[j];
+    else
+      return xs_[i] > xs_[j];
+  }
+};
+
+
+/**
+ * Return an integer array of indices of the specified container
+ * sorting the values in ascending or descending order based on
+ * the value of the first template prameter.
+ *
+ * @tparam ascending true if sort is in ascending order
+ * @tparam C type of container
+ * @param xs Container to sort
+ * @return sorted version of container
+ */
+template <bool ascending, typename C>
+std::vector<int> sort_indices(const C& xs) {
+  typename C::size_type size = xs.size();
+  std::vector<int> idxs;
+  idxs.resize(size);
+  for (typename C::size_type i = 0; i < size; ++i)
+    idxs[i] = i;
+  index_comparator<ascending,C> comparator(xs);
+  std::sort(idxs.begin(), idxs.end(), comparator);
+  return idxs;
 }
+
+
+// [[Rcpp::export]]
+IntegerVector sort_indices(NumericVector x){
+  return wrap(sort_indices<true>(as<std::vector<double> >(x)));
+}
+
 
 //' Cluster ranges which are implemented as 2 equal-length numeric vectors.
 //' @param starts A numeric vector that defines the starts of each interval
@@ -45,7 +100,9 @@ IntegerVector cluster_interval(NumericVector starts, NumericVector ends, int max
   IntegerVector result(starts.size());
   int cluster_id = 0;
   int prev_end = starts[0];
-  for (auto i: sort_indexes(clone(starts))) {
+  vector<int> indices = sort_indices<true>(as<std::vector<double> >(starts));
+  for (int j = 0; j < indices.size(); j++) {
+    int i = indices[j];
     Rcpp::checkUserInterrupt();
     if(starts[i] - prev_end > max_distance){
       cluster_id++;
